9f58a1c32ea6a4f3077ed1d1e78e7442205bde36
David Kochkin <kochkin.david@gmail.com>
2023-12-29 19:14:45 +0200

    Setup TabView

--- DubDubGrubClone/AppTabView.swift
+++ DubDubGrubClone/AppTabView.swift
@@ -10,8 +10,20 @@ import SwiftUI
 struct AppTabView: View {
     var body: some View {
         TabView {
-            
+            LocationMapView()
+                .tabItem {
+                    Label("Map", systemImage: "map")
+                }
+            LocationListView()
+                .tabItem {
+                    Label("Location", systemImage: "building")
+                }
+            ProfileView()
+                .tabItem {
+                    Label("Profile", systemImage: "person")
+                }
         }
+        .accentColor(.brandPrimary)
     }
 }
 
--- DubDubGrubClone/DubDubGrubCloneApp.swift
+++ DubDubGrubClone/DubDubGrubCloneApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct DubDubGrubCloneApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AppTabView()
         }
     }
 }
