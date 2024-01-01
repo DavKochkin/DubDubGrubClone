397ad70c3ce5098fbe313484bd505ce8fbec6a6e
David Kochkin <kochkin.david@gmail.com>
2024-01-01 16:57:56 +0200

    Add LocationDetailView

---
+++ DubDubGrubClone/LocationDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDetailView.swift
+//  DubDubGrubClone
+//
+//  Created by David Kochkin on 01.01.2024.
+//
+
+import SwiftUI
+
+struct LocationDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDetailView()
+}
