11cf0fe6b47b2c63499f562ce888c49fdd4254cf
David Kochkin <kochkin.david@gmail.com>
2024-01-08 14:10:21 +0200

    Add CloudKitManager

---
+++ DubDubGrubClone/Utilities/CloudKitManager.swift
@@ -0,0 +1,35 @@
+//
+//  CloudKitManager.swift
+//  DubDubGrubClone
+//
+//  Created by David Kochkin on 08.01.2024.
+//
+
+import CloudKit
+
+struct CloudKitManager {
+    
+    static func getLocations(completion: @escaping (Result<[DDGLocation], Error>) -> Void) {
+        let sortDescriptor = NSSortDescriptor(key: DDGLocation.kName, ascending: true)
+        let query = CKQuery(recordType: RecordType.location, predicate: NSPredicate(value: true))
+        query.sortDescriptors = [sortDescriptor]
+        
+        CKContainer.default().publicCloudDatabase.perform(query, inZoneWith: nil) { records, error in
+            guard error == nil else {
+                completion(.failure(error!))
+                return
+            }
+            
+            guard let records = records else { return }
+            
+            var locations: [DDGLocation] = []
+            
+            for record in records {
+                let location = DDGLocation(record: record)
+                locations.append(location)
+            }
+            
+            completion(.success(locations))
+        }
+    }
+}
