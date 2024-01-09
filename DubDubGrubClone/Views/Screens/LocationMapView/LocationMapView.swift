7e00713e56964a14f8e8c80bbdaf1719d91d01b5
David Kochkin <kochkin.david@gmail.com>
2024-01-09 17:04:40 +0200

    Add LocationMapViewModel

--- DubDubGrubClone/Views/Screens/LocationMapView/LocationMapView.swift
+++ DubDubGrubClone/Views/Screens/LocationMapView/LocationMapView.swift
@@ -10,19 +10,11 @@ import MapKit
 
 struct LocationMapView: View {
     
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(
-            latitude: 56.968,
-            longitude: 23.77038),
-        span: MKCoordinateSpan(
-            latitudeDelta: 0.01,
-            longitudeDelta: 0.01))
-    
-    @State private var alertItem: AlertItem?
+    @StateObject private var viewModel = LocationMapViewModel()
     
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $region)
+            Map(coordinateRegion: $viewModel.region)
                 .ignoresSafeArea()
             
             VStack {
@@ -32,21 +24,13 @@ struct LocationMapView: View {
                 Spacer()
             }
         }
-        .alert(item: $alertItem, content: { alertItem in
+        .alert(item: $viewModel.alertItem, content: { alertItem in
             Alert(title: alertItem.title,
                   message: alertItem.message,
                   dismissButton: alertItem.dismissButton)
         })
         .onAppear {
-            CloudKitManager.getLocations { result in
-                switch result {
-                    
-                case .success(let locations):
-                    print(locations)
-                case .failure(_):
-                    alertItem = AlertContext.unableToGetLocations
-                }
-            }
+            viewModel.getLocations()
         }
     }
 }
