621c8e14c0598f59ceb3d1ff0a90ce9279dd1dd4
David Kochkin <kochkin.david@gmail.com>
2024-01-11 12:50:30 +0200

    Add ImageDimension to Constants

--- DubDubGrubClone/Utilities/Constants.swift
+++ DubDubGrubClone/Utilities/Constants.swift
@@ -6,8 +6,24 @@
 //
 
 import Foundation
+import UIKit
+
 
 enum RecordType {
     static let location = "DDGLocation"
     static let profile  = "DDGProfile"
+}
+
+enum PlaceholderImage {
+    static let avatar = UIImage(named: "default-avatar")!
+    static let square = UIImage(named: "default-square-assey")!
+    static let banner = UIImage(named: "default-banner-asset")!
+}
+
+enum ImageDimension {
+    case square, banner
+    
+    func getPlaceholder(for dimension: ImageDimension) -> UIImage {
+        return dimension == .square ? PlaceholderImage.square : PlaceholderImage.banner
+    }
 }
