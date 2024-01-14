9bb321f92c5ea622215ffdbc3441105e31fe6cd2
David Kochkin <kochkin.david@gmail.com>
2024-01-14 19:39:36 +0200

    Add OnboardView

---
+++ DubDubGrubClone/Views/Screens/OnboardView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardView.swift
+//  DubDubGrubClone
+//
+//  Created by David Kochkin on 14.01.2024.
+//
+
+import SwiftUI
+
+struct OnboardView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    OnboardView()
+}
