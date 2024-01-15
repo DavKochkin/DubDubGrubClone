60a515f097e935ac10838f19ec135b0b2261617c
David Kochkin <kochkin.david@gmail.com>
2024-01-15 21:39:54 +0200

    Setup onboardView

--- DubDubGrubClone/Views/Screens/OnboardView.swift
+++ DubDubGrubClone/Views/Screens/OnboardView.swift
@@ -10,11 +10,51 @@ import SwiftUI
 struct OnboardView: View {
     var body: some View {
         VStack {
+            LogoView(frameWidth: 250)
+                .padding(.bottom)
             
+            VStack(alignment: .leading, spacing: 32) {
+                OnboardInfoView(imageName: "building.2.crop.circle",
+                                titleName: "Restaurant Locations",
+                                descriptionName: "Find places to dine around the convention center.")
+                
+                OnboardInfoView(imageName: "checkmark.circle",
+                                titleName: "Check In",
+                                descriptionName: "Let the other iOS Devs know where you are")
+                
+                OnboardInfoView(imageName: "person.2.circle",
+                                titleName: "Find Friends",
+                                descriptionName: "See where other iOS Devs are and join the party.")
+            }
+            .padding(.horizontal, 40)
         }
     }
 }
 
 #Preview {
     OnboardView()
+}
+
+struct OnboardInfoView: View {
+    
+    var imageName: String
+    var titleName: String
+    var descriptionName: String
+    
+    var body: some View {
+        HStack(spacing: 26) {
+            Image(systemName: imageName)
+                .resizable()
+                .frame(width: 50, height: 50)
+                .foregroundStyle(.brandPrimary)
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text(titleName).bold()
+                Text(descriptionName)
+                    .foregroundStyle(.secondary)
+                    .lineLimit(2)
+                    .minimumScaleFactor(0.75)
+            }
+        }
+    }
 }
