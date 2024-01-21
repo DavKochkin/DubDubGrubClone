57d0d64e5883e7ba6bfae62fe70c4549186f1966
David Kochkin <kochkin.david@gmail.com>
2024-01-21 11:06:08 +0200

    Add ProfileViewModel

---
+++ DubDubGrubClone/Views/Screens/ProfileView/ProfileView.swift
@@ -0,0 +1,124 @@
+//
+//  ProfileView.swift
+//  DubDubGrubClone
+//
+//  Created by David Kochkin on 29.12.2023.
+//
+
+import SwiftUI
+import CloudKit
+
+struct ProfileView: View {
+    
+    @StateObject private var viewModel = ProfileViewModel()
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                NameBackgroundView()
+                
+                HStack(spacing: 16) {
+                    ZStack {
+                        AvatarView(image: viewModel.avatar, size: 84)
+                        EditImage()
+                    }
+                    .padding(.leading, 12)
+                    .onTapGesture { viewModel.isShowingPhotoPicker = true }
+                    
+                    VStack(spacing: 1) {
+                        TextField.init("First Name", text: $viewModel.firstName)
+                            .profileNameStyle()
+                        
+                        TextField.init("Last Name", text: $viewModel.lastName)
+                            .profileNameStyle()
+                        
+                        TextField.init("Company Name", text: $viewModel.companyName)
+                    }
+                    .padding(.trailing, 16)
+                }
+                .padding()
+            }
+            
+            VStack(alignment: .leading, spacing: 6) {
+                CharactersRemainView(currentCount: viewModel.bio.count)
+                
+                TextEditor(text: $viewModel.bio)
+                    .frame(height: 100)
+                    .overlay(RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.secondary, lineWidth: 1))
+            }
+            .padding(.horizontal, 20)
+            
+            Spacer()
+            
+            Button {
+                //                createProfile()
+            } label: {
+                DDGButton(title: "Create Profile")
+            }
+            .padding(.bottom)
+        }
+        .navigationTitle("Profile")
+        .toolbar {
+            Button {
+                dismissKeyboard()
+            } label: {
+                Image(systemName: "keyboard.chevron.compact.down")
+            }
+        }
+        .onAppear { viewModel.getProfile() }
+        .alert(item: $viewModel.alertItem, content: { alertItem in
+            Alert(title: alertItem.title,
+                  message: alertItem.message,
+                  dismissButton: alertItem.dismissButton)
+        })
+        .sheet(isPresented: $viewModel.isShowingPhotoPicker) {
+            PhotoPicker(image: $viewModel.avatar)
+        }
+    }
+}
+
+#Preview {
+    NavigationView {
+        ProfileView()
+    }
+}
+
+struct NameBackgroundView: View {
+    var body: some View {
+        Color(.secondarySystemBackground)
+            .frame(height: 130)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding(.horizontal)
+    }
+}
+
+struct EditImage: View {
+    var body: some View {
+        Image(systemName: "square.and.pencil")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 14, height: 14)
+            .foregroundStyle(.white)
+            .offset(y: 30)
+    }
+}
+
+struct CharactersRemainView: View {
+    
+    var currentCount: Int
+    
+    var body: some View {
+        Text("Bio: ")
+            .font(.callout)
+            .foregroundStyle(.secondary)
+        +
+        Text("\(100 - currentCount)")
+            .font(.callout)
+            .foregroundStyle(currentCount <= 100 ? .brandPrimary : Color(.systemPink))
+        +
+        Text(" Characters Remain")
+            .font(.callout)
+            .foregroundStyle(.secondary)
+    }
+}
--- DubDubGrubClone/Views/Screens/ProfileView/ProfileViewModel.swift
+++ DubDubGrubClone/Views/Screens/ProfileView/ProfileViewModel.swift
@@ -1,87 +1,22 @@
 //
-//  ProfileView.swift
+//  ProfileViewModel.swift
 //  DubDubGrubClone
 //
-//  Created by David Kochkin on 29.12.2023.
+//  Created by David Kochkin on 21.01.2024.
 //
 
-import SwiftUI
+import Foundation
 import CloudKit
 
-struct ProfileView: View {
+final class ProfileViewModel: ObservableObject {
     
-    @State private var firstName   = ""
-    @State private var lastName    = ""
-    @State private var companyName = ""
-    @State private var bio         = ""
-    @State private var avatar      = PlaceholderImage.avatar
-    @State private var isShowingPhotoPicker = false
-    @State private var alertItem: AlertItem?
-    
-    var body: some View {
-        VStack {
-            ZStack {
-                NameBackgroundView()
-                
-                HStack(spacing: 16) {
-                    ZStack {
-                        AvatarView(image: avatar, size: 84)
-                        EditImage()
-                    }
-                    .padding(.leading, 12)
-                    .onTapGesture { isShowingPhotoPicker = true }
-                    
-                    VStack(spacing: 1) {
-                        TextField.init("First Name", text: $firstName)
-                            .profileNameStyle()
-                        
-                        TextField.init("Last Name", text: $lastName)
-                            .profileNameStyle()
-                        
-                        TextField.init("Company Name", text: $companyName)
-                    }
-                    .padding(.trailing, 16)
-                }
-                .padding()
-            }
-            
-            VStack(alignment: .leading, spacing: 6) {
-                CharactersRemainView(currentCount: bio.count)
-                
-                TextEditor(text: $bio)
-                    .frame(height: 100)
-                    .overlay(RoundedRectangle(cornerRadius: 8)
-                        .stroke(Color.secondary, lineWidth: 1))
-            }
-            .padding(.horizontal, 20)
-            
-            Spacer()
-            
-            Button {
-//                createProfile()
-            } label: {
-                DDGButton(title: "Create Profile")
-            }
-            .padding(.bottom)
-        }
-        .navigationTitle("Profile")
-        .toolbar {
-            Button {
-                dismissKeyboard()
-            } label: {
-                Image(systemName: "keyboard.chevron.compact.down")
-            }
-        }
-        .onAppear { getProfile() }
-        .alert(item: $alertItem, content: { alertItem in
-            Alert(title: alertItem.title,
-                  message: alertItem.message,
-                  dismissButton: alertItem.dismissButton)
-        })
-        .sheet(isPresented: $isShowingPhotoPicker) {
-            PhotoPicker(image: $avatar)
-        }
-    }
+    @Published var firstName   = ""
+    @Published var lastName    = ""
+    @Published var companyName = ""
+    @Published var bio         = ""
+    @Published var avatar      = PlaceholderImage.avatar
+    @Published var isShowingPhotoPicker = false
+    @Published var alertItem: AlertItem?
     
     func isValidProfile() -> Bool {
         guard !firstName.isEmpty,
@@ -90,8 +25,8 @@ struct ProfileView: View {
               !bio.isEmpty,
               avatar != PlaceholderImage.avatar,
               bio.count < 100 else { return false }
-                
-                return true
+        
+        return true
     }
     
     func createProfile() {
@@ -161,7 +96,7 @@ struct ProfileView: View {
                         return
                     }
                     
-                    DispatchQueue.main.async {
+                    DispatchQueue.main.async { [self] in
                         let profile = DDGProfile(record: profileRecord)
                         firstName   = profile.firstName
                         lastName    = profile.lastName
@@ -172,50 +107,5 @@ struct ProfileView: View {
                 }
             }
         }
-    }
-}
-
-#Preview {
-    NavigationView {
-        ProfileView()
-    }
-}
-
-struct NameBackgroundView: View {
-    var body: some View {
-        Color(.secondarySystemBackground)
-            .frame(height: 130)
-            .clipShape(RoundedRectangle(cornerRadius: 12))
-            .padding(.horizontal)
-    }
-}
-
-struct EditImage: View {
-    var body: some View {
-        Image(systemName: "square.and.pencil")
-            .resizable()
-            .scaledToFit()
-            .frame(width: 14, height: 14)
-            .foregroundStyle(.white)
-            .offset(y: 30)
-    }
-}
-
-struct CharactersRemainView: View {
-    
-    var currentCount: Int
-    
-    var body: some View {
-        Text("Bio: ")
-            .font(.callout)
-            .foregroundStyle(.secondary)
-        +
-        Text("\(100 - currentCount)")
-            .font(.callout)
-            .foregroundStyle(currentCount <= 100 ? .brandPrimary : Color(.systemPink))
-        +
-        Text(" Characters Remain")
-            .font(.callout)
-            .foregroundStyle(.secondary)
     }
 }
