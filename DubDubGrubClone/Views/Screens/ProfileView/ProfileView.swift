5035af812434b6f266d45a3f59c643c5ba273276
David Kochkin <kochkin.david@gmail.com>
2024-01-23 12:02:28 +0200

    Creat LoadingView

--- DubDubGrubClone/Views/Screens/ProfileView/ProfileView.swift
+++ DubDubGrubClone/Views/Screens/ProfileView/ProfileView.swift
@@ -13,46 +13,50 @@ struct ProfileView: View {
     @StateObject private var viewModel = ProfileViewModel()
     
     var body: some View {
-        VStack {
-            ZStack {
-                NameBackgroundView()
-                
-                HStack(spacing: 16) {
-                    ZStack {
-                        AvatarView(image: viewModel.avatar, size: 84)
-                        EditImage()
-                    }
-                    .padding(.leading, 12)
-                    .onTapGesture { viewModel.isShowingPhotoPicker = true }
+        ZStack {
+            VStack {
+                ZStack {
+                    NameBackgroundView()
                     
-                    VStack(spacing: 1) {
-                        TextField.init("First Name", text: $viewModel.firstName) .profileNameStyle()
-                        TextField.init("Last Name", text: $viewModel.lastName).profileNameStyle()
-                        TextField.init("Company Name", text: $viewModel.companyName)
+                    HStack(spacing: 16) {
+                        ZStack {
+                            AvatarView(image: viewModel.avatar, size: 84)
+                            EditImage()
+                        }
+                        .padding(.leading, 12)
+                        .onTapGesture { viewModel.isShowingPhotoPicker = true }
+                        
+                        VStack(spacing: 1) {
+                            TextField.init("First Name", text: $viewModel.firstName) .profileNameStyle()
+                            TextField.init("Last Name", text: $viewModel.lastName).profileNameStyle()
+                            TextField.init("Company Name", text: $viewModel.companyName)
+                        }
+                        .padding(.trailing, 16)
                     }
-                    .padding(.trailing, 16)
+                    .padding()
                 }
-                .padding()
-            }
-            
-            VStack(alignment: .leading, spacing: 6) {
-                CharactersRemainView(currentCount: viewModel.bio.count)
                 
-                TextEditor(text: $viewModel.bio)
-                    .frame(height: 100)
-                    .overlay(RoundedRectangle(cornerRadius: 8)
-                        .stroke(Color.secondary, lineWidth: 1))
+                VStack(alignment: .leading, spacing: 6) {
+                    CharactersRemainView(currentCount: viewModel.bio.count)
+                    
+                    TextEditor(text: $viewModel.bio)
+                        .frame(height: 100)
+                        .overlay(RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.secondary, lineWidth: 1))
+                }
+                .padding(.horizontal, 20)
+                
+                Spacer()
+                
+                Button {
+                    viewModel.createProfile()
+                } label: {
+                    DDGButton(title: "Create Profile")
+                }
+                .padding(.bottom)
             }
-            .padding(.horizontal, 20)
+            LoadingView()
             
-            Spacer()
-            
-            Button {
-                viewModel.createProfile()
-            } label: {
-                DDGButton(title: "Create Profile")
-            }
-            .padding(.bottom)
         }
         .navigationTitle("Profile")
         .toolbar {
