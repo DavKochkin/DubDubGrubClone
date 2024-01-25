a25b6ba3cd2b2f988066ec8c30218ac390b8bc50
David Kochkin <kochkin.david@gmail.com>
2024-01-25 20:58:08 +0200

    Add LocationDetailVM

---
+++ DubDubGrubClone/Views/Screens/LocationDetailView/LocationDetailViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  LocationDetailViewModel.swift
+//  DubDubGrubClone
+//
+//  Created by David Kochkin on 25.01.2024.
+//
+
+import Foundation
+
+final class LocationDetailViewModel: ObservableObject {
+    
+}
