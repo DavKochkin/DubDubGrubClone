dc66423424fa7f96917ceb4bdabe782daedc826a
David Kochkin <kochkin.david@gmail.com>
2024-02-20 14:20:31 +0200

    Refactor AppTabVM

--- DubDubGrubClone/Views/Screens/AppTabView/AppTabView.swift
+++ DubDubGrubClone/Views/Screens/AppTabView/AppTabView.swift
@@ -26,11 +26,9 @@ struct AppTabView: View {
         }
         .onAppear {
             CloudKitManager.shared.getUserRecord()
-            viewModel.runStartupChecks()
+            viewModel.checkIfHasSeenOnboard()
         }
-        .sheet(isPresented: $viewModel.isShowingOnboardView, onDismiss: viewModel.checkIfLocationServicesIsEnabled) {
-            OnboardView()
-        }
+        .sheet(isPresented: $viewModel.isShowingOnboardView) { OnboardView() }
     }
 }
 
--- DubDubGrubClone/Views/Screens/AppTabView/AppTabViewModel.swift
+++ DubDubGrubClone/Views/Screens/AppTabView/AppTabViewModel.swift
@@ -4,62 +4,21 @@
 //
 //  Created by David Kochkin on 05.02.2024.
 //
-
-import CoreLocation
 import SwiftUI
 
 extension AppTabView {
     
-    final class AppTabViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    final class AppTabViewModel: ObservableObject {
         @Published var isShowingOnboardView = false
-        @Published var alertItem: AlertItem?
         @AppStorage("hasSeenOnboardView") var hasSeenOnboardView = false {
             didSet { isShowingOnboardView = hasSeenOnboardView}
         }
-        
-        var deviceLocationManager: CLLocationManager?
+    
         let kHasSeenOnboardView = "hasSeenOnboardView"
         
         
-        func runStartupChecks() {
-            if !hasSeenOnboardView {
-                hasSeenOnboardView = true
-            } else {
-                checkIfLocationServicesIsEnabled()
-            }
-        }
-        
-        
-        func checkIfLocationServicesIsEnabled() {
-            if CLLocationManager.locationServicesEnabled() {
-                deviceLocationManager = CLLocationManager()
-                deviceLocationManager!.delegate = self
-            } else {
-                alertItem = AlertContext.locationDisabled
-            }
-        }
-        
-        
-        private func checkLocationAuthorization() {
-            guard let deviceLocationManager = deviceLocationManager else { return }
-            
-            switch deviceLocationManager.authorizationStatus {
-            case .notDetermined:
-                deviceLocationManager.requestWhenInUseAuthorization()
-            case .restricted:
-                alertItem = AlertContext.locationRestricted
-            case .denied:
-                alertItem = AlertContext.locationDenied
-            case .authorizedAlways, .authorizedWhenInUse:
-                break
-            @unknown default:
-                break
-            }
-        }
-        
-        
-        func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-            checkLocationAuthorization()
+        func checkIfHasSeenOnboard() {
+            if !hasSeenOnboardView { hasSeenOnboardView = true }
         }
     }
 }
