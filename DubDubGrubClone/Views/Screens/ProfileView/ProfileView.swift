6080534ee2d9d9b46669ee419cd064fc555ddad2
David Kochkin <kochkin.david@gmail.com>
2024-02-24 23:08:00 +0700

    Keyboard UX Improvements

--- DubDubGrubClone/Views/Screens/ProfileView/ProfileView.swift
+++ DubDubGrubClone/Views/Screens/ProfileView/ProfileView.swift
@@ -11,6 +11,11 @@ import CloudKit
 struct ProfileView: View {
     
     @StateObject private var viewModel = ProfileViewModel()
+    @FocusState private var focusedTextField: ProfileTextField?
+    
+    enum ProfileTextField {
+        case firstName, lastName, companyName, bio
+    }
     
     var body: some View {
         ZStack {
@@ -21,8 +26,21 @@ struct ProfileView: View {
                     
                     VStack(spacing: 1) {
                         TextField.init("First Name", text: $viewModel.firstName) .profileNameStyle()
+                            .profileNameStyle()
+                            .focused($focusedTextField, equals: .firstName)
+                            .onSubmit { focusedTextField = .lastName }
+                            .submitLabel(.next)
+                        
                         TextField.init("Last Name", text: $viewModel.lastName).profileNameStyle()
+                            .profileNameStyle()
+                            .focused($focusedTextField, equals: .lastName)
+                            .onSubmit { focusedTextField = .companyName }
+                            .submitLabel(.next)
+                        
                         TextField.init("Company Name", text: $viewModel.companyName)
+                            .focused($focusedTextField, equals: .companyName)
+                            .onSubmit { focusedTextField = .bio}
+                            .submitLabel(.next)
                     }
                     .padding(.trailing, 16)
                 }
@@ -50,6 +68,8 @@ struct ProfileView: View {
                     }
                     
                     BioTextEditor(text: $viewModel.bio)
+                        .focused($focusedTextField, equals: .bio)
+                    
                 }
                 .padding(.horizontal, 20)
                 
@@ -62,19 +82,18 @@ struct ProfileView: View {
                 }
                 .padding(.bottom)
             }
+            .toolbar {
+                ToolbarItem(placement: .keyboard) {
+                    Button("Dismiss") { focusedTextField = nil }
+                }
+            }
             
             if viewModel.isLoading { LoadingView() }
             
         }
         .navigationTitle("Profile")
         .navigationBarTitleDisplayMode(DeviceTypes.isiPhone8Standard ? .inline : .automatic)
-        .toolbar {
-            Button {
-                dismissKeyboard()
-            } label: {
-                Image(systemName: "keyboard.chevron.compact.down")
-            }
-        }
+        .ignoresSafeArea(.keyboard)
         .task {
             viewModel.getProfile()
             viewModel.getCheckedInStatus()
